63da1727c399512ece4c9cf599f7a295ea0fc6a9
zchuhui <943339564@qq.com>
2017-08-18 17:59:53 +0800

    产品对比

--- src/pages/sale-secy/views/goods-detail/goods-detail.js
+++ src/pages/sale-secy/views/goods-detail/goods-detail.js
@@ -7,6 +7,7 @@
 import React from 'react';
 import styles from './goods-detail.less'
 import moment from 'moment';
+import echarts from 'echarts';
 import { Button, Icon, DatePicker, Select, Tag, Cascader } from 'antd';
 
 const { MonthPicker, RangePicker } = DatePicker;
@@ -20,11 +21,6 @@ class GoodsDetail extends React.Component {
     }
     
     render() {
-
-        const children = [];
-        for (let i = 10; i < 36; i++) {
-          children.push(<Option key={i}>{'属性' + i}</Option>);
-        }
 
         return (
             <div className={`${styles.mainWrap} ${styles.goodsDetailWrap}`}>
@@ -106,32 +102,141 @@ class GoodsDetail extends React.Component {
                             <span className={styles.lateDate} >最近15天</span>
                             <span className={styles.lateDate} >最近30天</span>
                             <span className={styles.lateDate} >Life Time</span>
-                            
                         </div>
-                        <div>
-                            {/* <span>选择属性：</span> */}
-                            <Select
-                                mode="multiple"
-                                style={{ width: 510, marginTop:15 }}
-                                placeholder="Please select"
-                                defaultValue={['属性0']}
-                                onChange={handleChange}
-                            >
-                                {children}
-                            </Select>
+                        <div className={styles.clear}>
+                            <div className={styles.echartBox} >
+                                <div className={styles.changeAttr}>
+                                    <Select className={styles.select} defaultValue="属性1" onChange={this.handleChange}>
+                                        <Option value="1">属性1</Option>
+                                        <Option value="2">属性2</Option>
+                                        <Option value="3">属性3</Option>
+                                    </Select>
+                                    <Select className={styles.select} defaultValue="属性1" onChange={this.handleChange}>
+                                        <Option value="1">属性1</Option>
+                                        <Option value="2">属性2</Option>
+                                        <Option value="3">属性3</Option>
+                                    </Select>
+                                    <Select className={styles.select} defaultValue="属性1" onChange={this.handleChange}>
+                                        <Option value="1">属性1</Option>
+                                        <Option value="2">属性2</Option>
+                                        <Option value="3">属性3</Option>
+                                    </Select>
+                                </div>
+                                <div ref="chartBG" style={{width:'100%',height:250}}></div>
+                            </div>
+                            <div className={styles.echartBox} >
+                                <div className={styles.changeAttr}>
+                                    <Select className={styles.select} defaultValue="属性1" onChange={this.handleChange}>
+                                        <Option value="1">属性1</Option>
+                                        <Option value="2">属性2</Option>
+                                        <Option value="3">属性3</Option>
+                                    </Select>
+                                    <Select className={styles.select} defaultValue="属性1" onChange={this.handleChange}>
+                                        <Option value="1">属性1</Option>
+                                        <Option value="2">属性2</Option>
+                                        <Option value="3">属性3</Option>
+                                    </Select>
+                                    <Select className={styles.select} defaultValue="属性1" onChange={this.handleChange}>
+                                        <Option value="1">属性1</Option>
+                                        <Option value="2">属性2</Option>
+                                        <Option value="3">属性3</Option>
+                                    </Select>
+                                </div>
+                                <div ref="chartCompete" style={{width:'100%',height:250}}></div>
+                            </div>
                         </div>
-
 
                     </div>
                 </div>
             </div>
         );
-
-        function handleChange(value) {
-            console.log(`selected ${value}`);
-        }
+        
     }
 
+    handleChange(value) {
+        console.log(`selected ${value}`);
+    }
+
+    loadChart() {
+        
+        const chartBG = echarts.init(this.refs.chartBG),
+              chartCompete = echarts.init(this.refs.chartCompete);
+        
+        const option = {
+            title: {
+            },
+            tooltip: {
+                trigger: 'axis',
+            },
+            legend: {
+            },
+            toolbox: {
+                feature: {
+                }
+            },
+            grid: {
+                top: '3%',
+                left: '3%',
+                right: '4%',
+                bottom: '3%',
+                containLabel: false,
+            },
+            xAxis: [
+                {
+                    type: 'category',
+                    boundaryGap: true,
+                    data : ['周一','周二','周三','周四','周五','周六','周日'],
+                    show: true,
+                    axisLabel: {
+                        show: true,
+                    },
+                    axisLine: {
+                        lineStyle: {
+                            color: '#acdaff'    // x轴颜色
+                        }
+                    }
+                }
+            ],
+            yAxis: [
+                {
+                    show: true,
+                    splitLine: {
+                        show: false
+                    },
+                    axisLabel: {
+                        show: true,
+                    },
+                    axisLine: {
+                        lineStyle: {
+                            color: '#acdaff'    // y轴颜色
+                        }
+                    }
+                }
+            ],
+            series: [
+                {
+                    name: '价格',
+                    type: 'line',
+                    stack: '总量',
+                    itemStyle: {
+                        normal: {
+                            color: '#acdaff',
+                            show: true,
+                        },
+                    },
+                    areaStyle: { normal: {} },
+                    data:[120, 132, 101, 134, 90, 230, 210]
+                }
+            ]
+        }
+
+        chartBG.setOption(option);
+        chartCompete.setOption(option);
+    }
+
+    componentDidMount(){
+        this.loadChart();
+    }
 
 }
 
